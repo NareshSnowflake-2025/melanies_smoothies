abad3862f2c35354b4ac080872683b1cdba35e73
NareshSnowflake-2025 <nareshkumark@hexaware.com>
2025-04-20 10:14:16 +0530

    Update Streamlit_aap.py

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -20,7 +20,8 @@ if ingredients_list:
  Ingredients_string =''
  for fruit_chosen in ingredients_list:
     Ingredients_string += fruit_chosen + ' '
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+    st.subheader(fruit_chosen +  'Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(Ingredients_string)
 
