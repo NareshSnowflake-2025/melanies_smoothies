25a539cabe6121188be245e6400d94a075b56633
NareshSnowflake-2025 <nareshkumark@hexaware.com>
2025-04-20 10:48:34 +0530

    Update Streamlit_aap.py

--- Streamlit_aap.py
+++ Streamlit_aap.py
@@ -12,8 +12,9 @@ name_on_order = st.text_input("Name on Smoothie:")
 st.write('The name on your smoothie will be',name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERACH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop() 
 ingredients_list = st.multiselect("Choose up to 5 ingredients:",
   my_dataframe, max_selections=5)
 if ingredients_list:
@@ -22,7 +23,7 @@ if ingredients_list:
     Ingredients_string += fruit_chosen + ' '
     st.subheader(fruit_chosen +  'Nutrition Information')
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)  
     #st.write(Ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
